826ea4c7ec6a492f3a4bf38fff88003e08c0b623
Hansem Sohn <hansem@HansemMBP.local>
2021-05-06 17:29:44 -0400

    implementing parametric design of prey arm - work in progress

--- task/generate_stimuli/conditions/random/random.py
+++ task/generate_stimuli/conditions/random/random.py
@@ -2,29 +2,39 @@
 
 import numpy as np
 
-_min_length_range = 0.5  # in unit of screen size
-_max_length_range = 0.8
+_width = 10  # num of cells of grid; should match maze_lib_new/maze.Maze input
+_height = 10
+
+_min_length_range = 8  # in unit of cells
+_max_length_range = 25
 
 _min_num_turns_range = 1
 _max_num_turns_range = 3
 
+_min_arm_length = 3
+
 class Base():
-    """Single handcrafted maze.
-        TODO: implement randfixedsum (identical distribution across individual segments)
+    """generate prey path
+    random sampling of # turns and path length
+    rejection sampling if
+        1) path doesn't fit in grid (_w, _h),
+        2) segment length < _min_arm_length
+
+    algorithm forces the following conditions
+        1) side of end point is different from side of entry point (avoiding loop; complicating eye movement & leading to popup effect)
+        2) after sampling direction & arm length, move around path within grid to make entry and end point touch sides
 
     """
 
-    _NUM_SAMPLES_PER_NUM_TURNS = 10
-    _END = (0.5, 0.1)
+    _NUM_SAMPLES_PER_NUM_TURNS = 100
 
     def __init__(self,
                  length_range,
                  num_turns_range,
-                 name='',
-                 min_arm_length=0.1):
+                 name=''):
         self._length_range = length_range
         self._num_turns_range = num_turns_range
-        self._min_arm_length = min_arm_length
+        self._min_arm_length = _min_arm_length
         self._name = name
 
     def _random_num_turns(self):
@@ -32,64 +42,81 @@ class Base():
             self._num_turns_range[0], self._num_turns_range[1])
 
     def _sample_vertices(self, num_turns, length):
-        turn_points = np.random.uniform(0, length, size=(num_turns,))
+        turn_points = np.random.randint(0, length, size=(num_turns,))
         turn_points = np.sort(turn_points)
         vertices = np.concatenate(([0], turn_points, [length]))
         pairwise_distances = np.abs(
             vertices[:, np.newaxis] - vertices[np.newaxis, :])
         np.fill_diagonal(pairwise_distances, np.inf)
-        if np.sum(pairwise_distances < self._min_arm_length) > 0:
+        length_1 = np.sum(pairwise_distances[::2]) - (np.size(pairwise_distances[::2]) - 1)  # remove overlapped cell
+        length_2 = np.sum(pairwise_distances[1::2]) - (np.size(pairwise_distances[1::2]) - 1)
+        if np.sum(pairwise_distances < self._min_arm_length) > 0:  # rejection sampling
+            return self._sample_vertices(num_turns, length)
+        elf length_1 < _width:  # path doesn't fit in grid (_w, _h),
+            return self._sample_vertices(num_turns, length)
+        elf length_2 > _height:
             return self._sample_vertices(num_turns, length)
         else:
             return vertices
 
     def _sample_arm(self, num_turns, length, start_direction):
         vertices = self._sample_vertices(num_turns, length)
-        directions = [start_direction, 'u']
-        for i in range(1, num_turns):
-            if i % 2 == 0:
-                directions.append('u')
-            else:
-                directions.append(np.random.choice(['l', 'r']))
-
         lengths = vertices[1:] - vertices[:-1]
+
+        # direction: choose horizontal if vertical (vice versa)
+        directions = [start_direction]
+        # initialize length_x length_y
+        if directions[-1] == 'r' or directions[-1] == 'l':
+            length_x = lengths[0]
+            length_y = [0]
+        if directions[-1] == 'u' or directions[-1] == 'd':
+            length_x = [0]
+            length_y = lengths[0]
+        for i in range(1, num_turns):
+            if directions[-1] == 'r' or directions[-1] == 'l':
+                directions.append(np.random.choice(['u', 'd']))
+                length_y += lengths[i]
+            if directions[-1] == 'u' or directions[-1] == 'd':
+                directions.append(np.random.choice(['r', 'l']))
+                length_x += lengths[i]
+
+        # after sampling direction & arm length, move around path within grid to make entry and end point touch sides
+        # _END = (0.5, 0.1)
+
+        # first sum up hor/ver length
+        odd = lengths[::2]
+
+        even = lengths[1::2]
+
+
+
         arm = {
             'end': Base._END,
             'directions': ''.join(directions),
             'lengths': lengths.tolist(),
         }
 
-        return arm
+        # convert arm into list of cell index
+
+        arm_full
+
+        return arm, arm_full
 
     def _sample_condition(self, num_turns):
-        prey_arm_length = np.random.uniform(
+        prey_arm_length = np.random.randint(
             self._length_range[0], self._length_range[1])
-        distractor_arm_length = np.random.uniform(
-            self._length_range[0], self._length_range[1])
-        
-        if np.random.rand() < 0.5:
-            prey_dir = 'l'
-            distractor_dir = 'r'
-        else:
-            prey_dir = 'r'
-            distractor_dir = 'l'
 
-        prey_arm = self._sample_arm(
+        prey_dir = np.random.choice(['l', 'r', 'u', 'd'])  # initial directions
+
+        prey_arm, prey_full_arm = self._sample_arm(
             num_turns, length=prey_arm_length, start_direction=prey_dir)
-        
-        distractor_num_turns = self._random_num_turns()
-        distractor_arm = self._sample_arm(
-            distractor_num_turns, length=distractor_arm_length,
-            start_direction=distractor_dir)
 
         features = {
             'name': self._name,
             'prey_arm_turns': num_turns,
             'prey_arm_length': prey_arm_length,
-            'distractor_arm_turns': distractor_num_turns,
-            'distractor_arm_length': distractor_arm_length,
         }
-        condition = [[prey_arm, distractor_arm], 0, features,]
+        condition = [[prey_arm, prey_full_arm], 0, features,]
         return condition
 
     def _conditions_per_num_turns(self, num_turns):
@@ -111,7 +138,7 @@ class Random3(Base):
     """Random with 1 to 3 prey turns."""
 
     def __init__(self):
-        length_range = [_min_length_range, _max_length_range]
+        length_range = [_min_length_range, _max_length_range+1]  # upper: exclusive
         num_turns_range = [_min_num_turns_range, _min_num_turns_range+1]  # upper: exclusive
         super(Random3, self).__init__(
             length_range, num_turns_range, name='Random3')
