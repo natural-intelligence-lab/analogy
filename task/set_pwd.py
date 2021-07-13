a433f933f739d4bf5dc6735c555a994ff1e42e26
Hansem <hansem@mit.edu>
2021-07-13 01:10:39 -0400

    fix set_pwd

--- task/set_pwd.py
+++ task/set_pwd.py
@@ -14,17 +14,30 @@ import os
 #     '/Users/nicholaswatters/miniconda3/envs/mworks_moog/lib/python3.8/site-packages'
 # )
 
-# laptop hansem
-_PWD = '/Users/hansem/Documents/MazeSetGo/task'
-_PYTHON_SITE_PACKAGES = (
-    '/opt/anaconda3/envs/mworks/lib/python3.8/site-packages'
-)
-
-# # desktop hansem
-# _PWD = '/Users/hansem/Documents/MazeSetGo/task'
-# _PYTHON_SITE_PACKAGES = (
-#     '/Users/hansem/miniconda3/envs/mworks/lib/python3.8/site-packages'
-# )
+if getvar('platform') == 'laptop':
+    _PWD = '/Users/hansem/Documents/MazeSetGo/task'
+    _PYTHON_SITE_PACKAGES = (
+        '/opt/anaconda3/envs/mworks/lib/python3.8/site-packages'
+    )
+elif getvar('platform') == 'desktop':
+    _PWD = '/Users/hansem/Documents/MazeSetGo/task'
+    _PYTHON_SITE_PACKAGES = (
+        '/Users/hansem/miniconda3/envs/mworks/lib/python3.8/site-packages'
+    )
+elif getvar('platform') == 'psychophysics':
+    # laptop hansem
+    _PWD = '/Users/hansem/Documents/MazeSetGo/task'
+    _PYTHON_SITE_PACKAGES = (
+        '/opt/anaconda3/envs/mworks/lib/python3.8/site-packages'
+    )
+elif getvar('platform') == 'monkey':
+    # laptop hansem
+    _PWD = '/Users/hansem/Documents/MazeSetGo/task'
+    _PYTHON_SITE_PACKAGES = (
+        '/Users/hansem/opt/anaconda3/envs/mworks/lib/python3.8/site-packages'
+    )
+else:
+    raise ValueError('Invalid platform')
 
 # Update pwd and python_site_packages variables in mworks
 setvar('pwd', _PWD)
