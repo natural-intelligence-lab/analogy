7431e4646bc3e968369baac413b377caea6ebf82
hansem <hansem@mit.edu>
2022-03-10 12:08:20 -0500

    now impose minimum overlap & min_distance_exits

--- task/configs/levels/random/wire_maze.py
+++ task/configs/levels/random/wire_maze.py
@@ -6,10 +6,14 @@ from configs import config
 from configs import samplers
 from configs.levels import get_stimuli_dir
 
-_min_num_turns = 2
-_max_num_turns = 6 # 4
-_step_num_turns = 2
-_num_layers= 2 # 50
+_MIN_NUM_TURNS = 2
+_MAX_NUM_TURNS = 6 # 4
+_STEP_NUM_TURNS = 2
+_NUM_LAYERS= 2 # 50
+
+_MIN_NUM_OVERLAP=1
+_MIN_EXIT_DISTANCE = 3  # GRID; if set to zero, no constraint on the exit distance
+
 
 def random_16(num_layers=4, max_num_turns=6, **kwargs):
     """Random layered mazes within a grid of size 16."""
@@ -24,7 +28,7 @@ def random_16(num_layers=4, max_num_turns=6, **kwargs):
     )
     return config.Config(stimulus_generator, **kwargs)
 
-def random_16_staircase(num_layers=_num_layers, max_num_turns=_max_num_turns,**kwargs):
+def random_16_staircase(**kwargs):
     """Random layered mazes within a grid of size 16."""
     path_dir = os.path.join(
         get_stimuli_dir.stimuli_dir(),
@@ -32,10 +36,12 @@ def random_16_staircase(num_layers=_num_layers, max_num_turns=_max_num_turns,**k
     )
     num_turns_samplers = [samplers.WireMazeSampler(
             path_dir=path_dir,
-            num_layers=num_layers,
+            num_layers=_NUM_LAYERS,
             ball_path_top_bottom=True,
             distractors_top_bottom=True,
-            max_num_turns=max_num_turns,
+            max_num_turns=_MAX_NUM_TURNS,
+            min_num_overlap=_MIN_NUM_OVERLAP,
+            min_exit_distance=_MIN_EXIT_DISTANCE,
         )
     ]
     stimulus_generator = samplers.MixtureSampler(*num_turns_samplers,
